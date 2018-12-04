2d8bdc539285b0ce910aa70060a02b98be180009
Yong Joseph Bakos <ybakos@humanoriented.com>
2018-12-04 12:26:52 -0800

    Users: Sign in after password change, if necessary.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,6 +34,7 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
+        sign_in(@user, :bypass => true) if @user == current_user
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
         format.json { render :show, status: :ok, location: @user }
       else
